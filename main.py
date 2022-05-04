2c5613c2e088aea90c0e72d3d251e9f8c49dc3db
Bukola Testimony <102736653+testimony01@users.noreply.github.com>
2022-05-04 22:13:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+#Write your code below this row 👇
+
+# Example 1:
+for number in range(1,101):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 5 == 0:
+    print("Buzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  else:
+      print(number)
+
+# Example 2:
+# for playlist in range(5, 62):
+#   # print(playlist)
+#   if playlist % 4 == 0 and playlist % 5 == 0:
+#     print("playlist")
+#   elif playlist % 5 == 0:
+#     print("play")
+#   elif playlist % 4 == 0:
+#     print("list")
+#   else:
+#     print(playlist)
+  
+ 
+        
